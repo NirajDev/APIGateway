2a358f4f66ecc450da7918153538f628c664c04e
Niraj <32629450+NirajDev@users.noreply.github.com>
2021-02-21 23:01:39 +0530

    Initial Commit

---
+++ src/main/java/com/tricket/apigateway/config/JungleeGatewayConfiguration.java
@@ -0,0 +1,20 @@
+package com.tricket.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class JungleeGatewayConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/authentication/**")
+                .permitAll()
+                .antMatchers("/**")
+                .authenticated();
+    }
+}
